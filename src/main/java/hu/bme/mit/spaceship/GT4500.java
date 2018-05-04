c904d9e84082cbdc2ffc04c140ee1226a68deaa6
Biró Balázs <balazs7726@gmail.com>
2018-05-03 23:59:16 -0700

    switch lecserélése if re

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -38,8 +38,8 @@ public class GT4500 implements SpaceShip {
 
     boolean firingSuccess = false;
 
-    switch (firingMode) {
-      case SINGLE:
+    if(firingMode==SINGLE) {
+      
         if (wasPrimaryFiredLast) {
           // try to fire the secondary first
           if (! secondaryTorpedoStore.isEmpty()) {
@@ -74,9 +74,9 @@ public class GT4500 implements SpaceShip {
             // if both of the stores are empty, nothing can be done, return failure
           }
         }
-        break;
-
-      case ALL:
+    }
+else{
+     
         // try to fire both of the torpedo stores
         //TODO implement feature
 	if (! secondaryTorpedoStore.isEmpty() && ! primaryTorpedoStore.isEmpty()) {
@@ -85,7 +85,6 @@ public class GT4500 implements SpaceShip {
             wasPrimaryFiredLast = false;
           }
 
-        break;
     }
 
     return firingSuccess;
